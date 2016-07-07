b9da05227e6bca91cc5b23b5d2f57fd0461c6a1c
aqjaffe <adamqjaffe@gmail.com>
2016-07-07 16:13:11 -0700

    Decomposition
    Decomposed files further, into two modules. Program is now ready to implement non-stochastic similarity methods!

---
+++ ranking.py
@@ -0,0 +1,47 @@
+# AUTHOR: Adam Jaffe
+# UPDATED: 6.29.16
+# ---------------------------------------------
+# This program is my first experiment in web-scraping!
+# It prompts the user for a song and artist and then collects
+# relevant chord data from www.ultimate-guitar.com to compile
+# a list of all of the user-generated chord sheets avaiable.
+# It transposes these all to the same key for easy comparison.
+
+from userIO import interface
+import compare
+
+### TODO: rewrite these method in terms of the song class
+### (currently they are written with outdated song-as-list protocol)
+
+def get_best_pairs(songs, num_results):
+    pairs = []
+    for i in range(len(songs)):
+        for j in range(len(songs) - i - 1):
+            element = [songs[i].title, songs[i].artist, songs[i + j + 1].title, songs[i + j + 1].artist]
+            results = compare.run_test_suite(songs[i], songs[i + j + 1], False)
+            sim = compare.get_average_results(results)
+            element.append(sim)
+            if len(pairs) < num_results:
+                pairs.append(element)
+                pairs.sort(key=lambda el: -el[4])
+            elif sim > pairs[num_results - 1][4]:
+                pairs.append(element)
+                pairs.sort(key=lambda el: -el[4])
+                pairs.pop()
+    return pairs
+
+def get_best_match(target_song, songs, num_results):
+    matches = []
+    for i in range(len(songs)):
+        element = [target_song.title, target_song.artist, songs[i].title, songs[i].artist]
+        results = compare.run_test_suite(songs[i], target_song, False)
+        sim = compare.get_average_results(results)
+        element.append(sim)
+        if len(matches) < num_results:
+            matches.append(element)
+            matches.sort(key=lambda el: -el[4])
+        elif sim > matches[num_results - 1][4]:
+            matches.append(element)
+            matches.sort(key=lambda el: -el[4])
+            matches.pop()
+    return matches
--- song.py
+++ song.py
@@ -2,35 +2,17 @@
 # ------------
 # This file defines the song class
 
-# the song class will contain the song's
-# -title
-# -artist
-# -chord sheet list
-# -markov table
-
-import scrape
 import music
-import markov
-import interface
+from userIO import scrape, interface
 
 class Song(object):
     def __init__(self, title, artist):
         self.title = title
         self.artist = artist
-        
-        self.title_url = scrape.simplify_str(title)
-        
-        self.chord_sheets = []
-        self.weights = []
-        
-        chord_sheet_urls = scrape.get_chord_sheet_urls(self.title_url, self.artist)
-        for url in chord_sheet_urls:
-            sheet_data = scrape.get_chord_sheet(url)
-            self.weights.append(sheet_data[0])
-            raw_chord_sheet = sheet_data[1]
-            processed_chord_sheet = music.process_chords(raw_chord_sheet)
-            self.chord_sheets.append(processed_chord_sheet)
-        self.markov_table = markov.make_markov_table(self.chord_sheets, self.weights)
+        chord_sheet_urls = scrape.get_chord_sheet_urls(scrape.simplify_str(title), self.artist)
+        sheets = scrape.urls_to_sheets(chord_sheet_urls)
+        self.chord_sheets = sheets[0]
+        self.weights = sheets[1]
     
     def __str__(self):
         return '\"' + self.title + '\" by ' + self.artist
